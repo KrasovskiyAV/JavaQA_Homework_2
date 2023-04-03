9da605848eea3f41aeadb7c03d5362620a1fc9a0
Красовский АВ <AV.Krasovskiy@ya.ru>
2023-04-03 21:03:24 +0700

    Main modified

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int price = 15897; // стоимость билета в рублях
+        int rubForMile = 20; // количество рублей для одной бонусной мили
+        int bonus = price / rubForMile; // количество бонусных миль
+        System.out.println("Количество бонусных миль - " + bonus);
     }
 }
